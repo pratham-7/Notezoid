c6bd14eaa5d74dffdd32c409a471a263403ed82f
pratham-7 <98410758+pratham-7@users.noreply.github.com>
2022-07-22 08:32:10 +0530

    Add files via upload

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+import Zoom from '@material-ui/core/Zoom';
+
+function CreateArea(props) {
+
+  const [isExpanded, setExpanded] = useState(false);
+
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote(prevNote => {
+      return {
+        ...prevNote,
+        [name]: value
+      }
+    })
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    })
+    setExpanded(false);
+    event.preventDefault();
+  }
+
+  function expand() {
+    setExpanded(true);
+  }
+
+  return (
+    <div>
+      <form className="create-note">
+        <input
+          name="title"
+          onClick={expand}
+          onChange={handleChange}
+          value={note.title}
+          placeholder={isExpanded ? "Input Title" : "Click to create a note!"}
+        />
+        {isExpanded && (
+          <textarea
+            name="content"
+            onChange={handleChange}
+            value={note.content}
+            placeholder="Input Text"
+            rows={isExpanded ? 3 : 1}
+          />
+        )}
+        <Zoom in={isExpanded}>
+          <Fab onClick={submitNote}><AddIcon /></Fab>
+        </Zoom>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      {/* eslint-disable-next-line */}
+      <h1>
+        Notezoid📝
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
